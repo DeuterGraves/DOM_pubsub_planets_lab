3ed7acfea84b869234afd4a5dea5b5611585b572
DeuterGraves <puckles@gmail.com>
2018-10-23 16:24:09 +0100

    user can click the planet list and a planet name is returned

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const planetsData = require('./data/planets.js');
 const SolarSystem = require('./models/solar_system.js');
+const MenuView = require('./views/menu_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-  // creates a new data model - pulls in planets data 
+  // creates a new data model - pulls in planets data
   const planetsDataModel = new SolarSystem(planetsData);
   console.log(planetsDataModel.planets);
+
+//  need to create a menu to be passed into the menu view ;)
+  const menu = document.querySelector('nav.planets-menu');
+  // load new menu_view and call bind events
+  const planetMenu  = new MenuView(menu);
+  planetMenu.bindEvents();
+
+  // load new details view and call bindevents
+
+  //
 });
---
+++ src/views/menu_view.js
@@ -0,0 +1,28 @@
+const PubSub = require("../helpers/pub_sub.js")
+
+
+// constructor
+// where 'planet' will be the planet selected by the user
+const MenuView = function(menu){
+  this.menu = menu;
+};
+
+// bindEvents
+MenuView.prototype.bindEvents = function(){
+
+  // listener for the menu clicked
+  this.menu.addEventListener("click", (menuClickedEvent) =>{
+    const planetName = menuClickedEvent.target.id;
+    console.log(planetName);
+    PubSub.publish("MenuView:planet-selected", planetName)
+    // publish the clicked planet out to a menu_view channel.
+
+  });
+  // check that the planets are present
+/*  PubSub.subscribe("Planets:all-planets-present", (planetsLoadedEvent) => {
+    const allPlanets = planetsLoadedEvent.detail;
+    */
+  };
+
+
+module.exports = MenuView;
